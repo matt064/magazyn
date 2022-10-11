ccfd7b66763fc8fb1af3fa1d5f137e4fd451a15c
Mateusz Gawroński <gawronski.m91@gmail.com>
2022-10-11 21:28:08 +0200

    fix buges

--- magazyn.py
+++ magazyn.py
@@ -1,26 +1,26 @@
 user_choice = -1
 #lista produktów
-items = ['Trimble_R2', 'Trimble_R8', 'Leica_ATX_1230', 'Leica_NA520',]
+#items = ['Trimble_R2', 'Trimble_R8', 'Leica_ATX_1230', 'Leica_NA520',]
 sold_items = {}
 
 #slownik produktów
 items_dict = {
-        'Trimble_R2' : {
+        'Trimble R2' : {
         'type': 'gps',
         'quantity': 3,
         'unit_price': 28_000,
         }, 
-    'Trimble_R8' : {
+    'Trimble R8' : {
             'type' : "gps",
             'quantity' : 5,
             'unit_price' : 14_000,
         }, 
-    'Leica_ATX_1230' : {
+    'Leica ATX 1230' : {
             'type': "gps",
             'quantity': 2,
             "unit_price" : 5_000,
         }, 
-    'Leica_NA520' : {
+    'Leica NA520' : {
             'type': "niwelator",
             "quantity" : 6,
             "unit_price" : 6_500,
@@ -43,13 +43,13 @@ def add_item():
     """dodaje nowy produkt do magazynu""" 
     new_item = input("Podaj nazwe produktu, ktory chcesz wprowadzic do bazy: ")
     type = input("Podaj typ produktu: ")
-    quantity = (input("Podaj ilość wprowadzanego produktu: "))
-    unit_price = (input("Podaj cene jednostkowa: "))
+    quantity = int((input("Podaj ilość wprowadzanego produktu: ")))
+    unit_price = int((input("Podaj cene jednostkowa: ")))
     
-    items.append(new_item)
+    
     items_dict.setdefault(new_item,{'type': type,'quantity': quantity,'unit_price': unit_price})
     print("Produkt wprowadzony")
-    return items
+    return items_dict
 
 def sell_item():  # dodac blad gdy wprowadzi sie wartosc inna niz w zbiorze
     "usuwa produkt z listy"
@@ -59,14 +59,45 @@ def sell_item():  # dodac blad gdy wprowadzi sie wartosc inna niz w zbiorze
     if delete_item in items_dict.keys():
         items_dict[delete_item]['quantity'] = items_dict[delete_item]['quantity'] - piece_of_product
         if items_dict[delete_item]['quantity'] == 0:
-            items.remove(delete_item)
             del items_dict[delete_item]
         elif items_dict[delete_item]['quantity'] < 0:
             print("Wprowadzona ilość sztuk do sprzedazy jest wieksza niz posiadana w magazynie! Operacja nieudana.")
-
-    sold_items = items_dict[delete_item].copy
+        else:
+            sold_items[delete_item] = items_dict[delete_item].copy()
+            sold_items[delete_item]['quantity'] = piece_of_product
 
     return items_dict, sold_items
+
+def get_costs():
+    """zlicza wartosc przedmiotow w magazynie"""
+    lista_wartosci = []
+    for name in items_dict.keys():
+        wartosc = items_dict[name]['quantity'] * items_dict[name]['unit_price'] 
+        lista_wartosci.append(wartosc)
+    calkowita_wartosc = sum(lista_wartosci)
+    return calkowita_wartosc
+    #print("Wartosc produktów w magazynie wynosci:", calkowita_wartosc)
+
+def get_income():
+    """zlicza wartosc sprzedanych produktow"""
+    lista_sprzedazy = []
+    for name in sold_items.keys():
+        wartosc = sold_items[name]['quantity'] * sold_items[name]['unit_price']
+        lista_sprzedazy.append(wartosc)
+    suma_sprzedaz = sum(lista_sprzedazy)
+    return suma_sprzedaz
+    #print("Zysk ze sprzedazy wynosci:", suma_sprzedaz)
+
+def show_revenue():
+    wartosc_magazynu = get_costs()
+    wartosc_sprzedazy = get_income()
+
+    print("Wartość produktów w magazynie wynosi:", wartosc_magazynu,"[PLN]")
+    print("Zysk ze sprzedanych produktów wynosi:", wartosc_sprzedazy,"[PLN]")
+
+    bilans =  wartosc_sprzedazy - wartosc_magazynu
+    print("Bilans (wartosc ze sprzedanych produktów - koszt ich zakupu) wynosi:", bilans,"[PLN]")
+
 
 
 
@@ -85,8 +116,8 @@ while True:
         sell_item()
 
     if user_choice == "4":
-        print(sold_items)
-    
+        show_revenue()
+        
     if user_choice == 'exit':
         print("Zamykam program, do widzenia.")
         break
@@ -96,7 +127,7 @@ while True:
     print("1. Wyświetl produkty w magazynie ")
     print("2. Dodaj produkt do magazynu.")
     print("3. Sprzedaż towaru z magazynu.")
-    print("4. Lista sprzedanych produktów")
+    print("4. Bilans zysków i kosztów")
 
 
     print("(możesz wyjsc z programu w każdym momencie, wpisując 'exit')")
--- proba.py
+++ proba.py
@@ -23,13 +23,51 @@ items_dict = {
             "unit_price" : 6_500,
         },
 }
+"""
+def sell_item():  # dodac blad gdy wprowadzi sie wartosc inna niz w zbiorze
+    "usuwa produkt z listy"
+    delete_item = input("Wprowadź nazwe produktu, ktory chcesz sprzedac: ")
+    piece_of_product = int(input("Podaj ile sztuk produktu chcesz sprzedac: "))
 
+    if delete_item in items_dict.keys():
+        items_dict[delete_item]['quantity'] = items_dict[delete_item]['quantity'] - piece_of_product
+        if items_dict[delete_item]['quantity'] == 0:
+            items.remove(delete_item)
+            del items_dict[delete_item]
+        elif items_dict[delete_item]['quantity'] < 0:
+            print("Wprowadzona ilość sztuk do sprzedazy jest wieksza niz posiadana w magazynie! Operacja nieudana.")
+"""
 sold_items = {}
-sold= 'Trimble_R2'
-piece = 2
+delete_item = input("wprwadz nazwe")
+piece_of_product = 2
 
 
-sold_items[sold] = items_dict[sold].copy()
-#sold_items['quantity'] = piece
+sold_items[delete_item] = items_dict[delete_item].copy()
+#sold_items[delete_item]['quantity'] = piece_of_product
 
-print(sold_items)
+
+print(sold_items[delete_item])
+
+
+
+
+
+
+
+
+
+
+# def get_costs():
+#     """zlicza wartosc przedmiotow w magazynie"""
+#     lista = []
+#     for name in items_dict.keys():
+#         wartosc = items_dict[name]['quantity'] * items_dict[name]['unit_price'] 
+#         print(wartosc)
+#         lista.append(wartosc)
+#     suma = sum(lista)
+#     print(suma)
+
+
+        
+# get_costs()
+# #print(wartosc)
